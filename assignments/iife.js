4a338de468cc598225b926f0006420f2efbf87cd
Kurt Van Etten <kurt@vanetten.us>
2019-01-19 01:58:35 -0500

    Make a simple IIFE example

--- assignments/iife.js
+++ assignments/iife.js
@@ -1,2 +1,25 @@
-// Put some examples of using IIFE here!
+"use strict";
 
+/*
+From MDN:
+
+An IIFE (Immediately Invoked Function Expression) is a JavaScript function that runs as soon as it is defined.
+
+(function () {
+    statements
+})();
+
+*/
+
+(() => console.log('Hello, World!'))();
+
+
+// use iife to create a counter
+
+const counter = (() => {let count = 0; return () => ++count;})();
+
+console.log(counter());
+console.log(counter());
+console.log(counter());
+
+// okay, that kind of makes my head hurt!
